236e27c9a4eae3113863524c43ea1944b74d7f8c
mohammadreza <moreh412000@gmial.com>
2024-04-18 09:44:55 +0330

    creating ship factory function

---
+++ logic.js
@@ -0,0 +1,17 @@
+function Ship() {
+  let length
+  let hitted = 0
+  return {
+    length: length,
+    hitted: hitted,
+    hit: function () {
+      this.hitted = this.hitted + 1
+    },
+    isSunk: function () {
+      if (this.hitted >= this.length) return true
+      return false
+    }
+  }
+}
+
+module.exports = { Ship }
