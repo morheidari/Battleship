b3d1014ac6ed58db2611b343e0bd5f453ffd01ef
mohammadreza <moreh412000@gmial.com>
2024-04-18 09:45:29 +0330

    test ship factory function

---
+++ __test__/ship.test.js
@@ -0,0 +1,30 @@
+const { Ship } = require('../logic.js')
+
+test('create a ship of length 3', () => {
+  const ship = Ship()
+  ship.length = 3
+  expect(ship.length).toBe(3)
+})
+
+test('hit a ship', () => {
+  const ship = Ship()
+  ship.length = 3
+  ship.hit()
+  expect(ship.hitted).toBe(1)
+})
+
+test('hit a ship of lenght three, three time so it is sunk', () => {
+  const ship = Ship()
+  ship.length = 3
+  ship.hit()
+  ship.hit()
+  ship.hit()
+  expect(ship.isSunk()).toBeTruthy()
+})
+
+test('hit a ship of length three one time so it is not sunk', () => {
+  const ship = Ship()
+  ship.length = 3
+  ship.hit()
+  expect(ship.isSunk()).toBeFalsy()
+})
