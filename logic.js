7c57c9bd9bad6988db6eff91b0ef5e1b323740bd
mohammadreza <moreh412000@gmial.com>
2024-04-20 15:19:28 +0330

    debug puShipInCordinate method in GameBoard Object

--- logic.js
+++ logic.js
@@ -16,9 +16,36 @@ function Ship(l) {
 
 function GameBoard() {
   let board = []
-  for (let i = 0; i < 10; i++) {
-    for (let j = 0; j < 10; j++) {
-      board[i][j] = null
+  function setupBoard() {
+    for (let i = 0; i < 10; i++) {
+      let row = []
+      for (let j = 0; j < 10; j++) {
+        row.push(null)
+      }
+      board.push(row)
+    }
+  }
+  setupBoard()
+  function isShipPlacable(ship, cordinates, orientation) {
+    const l = ship.length
+    if (orientation === 'horizontal') {
+      if (cordinates[0] + l - 1 < 10) {
+        for (let i = 0; i < l; i++) {
+          if (board[cordinates[0] + i][cordinates[1]] !== null) {
+            return false
+          }
+        }
+        return true
+      } else return false
+    } else {
+      if (cordinates[1] + l - 1 < 10) {
+        for (let i = 0; i < l; i++) {
+          if (board[cordinates[0]][cordinates[1] + i] !== null) {
+            return false
+          }
+        }
+        return true
+      } else return false
     }
   }
   return {
@@ -27,24 +54,20 @@ function GameBoard() {
     missedAttacks: [],
     putShipInCordinates: function (ship, cordinates, orientation) {
       const l = ship.length
-      if (orientation === 'horizontal') {
-        if (cordinates[0] + l - 1 < 10) {
+      if (isShipPlacable(ship, cordinates, orientation)) {
+        if (orientation === 'horizontal') {
           for (let i = 0; i < l; i++) {
-            boards[cordinates[0] + i][cordinates[1]] = ship
+            this.board[cordinates[0] + i][cordinates[1]] = ship
           }
-          return true
-        } else return false
-      } else {
-        if (cordinates[1] + l - 1 < 10) {
+        } else {
           for (let i = 0; i < l; i++) {
-            boards[cordinates[0]][cordinates[1] + i] = ship
+            this.board[cordinates[0]][cordinates[1] + i] = ship
           }
-          return true
-        } else return false
-      }
+        }
+      } else return isShipPlacable(ship, cordinates, orientation)
     },
     recieveAttack: function (cordinates) {
-      const c = board[cordinates[0]][cordinates[1]]
+      const c = this.board[cordinates[0]][cordinates[1]]
       if (c === null) {
         this.missedAttacks.push(cordinates)
       } else {
@@ -52,7 +75,7 @@ function GameBoard() {
       }
     },
     areAllShipsSunk: function () {
-      return this.ships.every((ship) => ship.isSunk)
+      return this.ships.every((ship) => ship.isSunk())
     }
   }
 }
