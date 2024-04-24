73d90ecc074abea1023b3fd587c438093863da54
mohammadreza <moreh412000@gmial.com>
2024-04-24 16:53:57 +0330

    add showAttackResult method

--- src/dom.js
+++ src/dom.js
@@ -39,7 +39,49 @@ function Render(identity) {
       }
     })
   }
-  return { showShipsPosition, showShipsCondition }
+  const missedAttacks = []
+  const succsessfullAttacks = []
+  function createCrossSvg() {
+    const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
+    svg.setAttribute('width', '100%')
+    svg.setAttribute('height', '100%')
+    svg.setAttribute('viewBox', '0 0 100 100')
+    const line1 = document.createElementNS('http://www.w3.org/2000/svg', 'line')
+    line1.setAttribute('x1', '0')
+    line1.setAttribute('y1', '0')
+    line1.setAttribute('x2', '100')
+    line1.setAttribute('y2', '100')
+    line1.setAttribute('stroke', 'black')
+    line1.setAttribute('stroke-width', '5')
+    const line2 = document.createElementNS('http://www.w3.org/2000/svg', 'line')
+    line2.setAttribute('x1', '0')
+    line2.setAttribute('y1', '100')
+    line2.setAttribute('x2', '100')
+    line2.setAttribute('y2', '0')
+    line2.setAttribute('stroke', 'black')
+    line2.setAttribute('stroke-width', '5')
+    const circle = document.createElementNS(
+      'http://www.w3.org/2000/svg',
+      'circle'
+    )
+    circle.setAttribute('cx', '50')
+    circle.setAttribute('cy', '50')
+    circle.setAttribute('r', '10')
+    circle.setAttribute('fill', 'black')
+    svg.appendChild(line1)
+    svg.appendChild(line2)
+    svg.appendChild(circle)
+    return svg
+  }
+  const showAttackResult = function (cordinates, gameBoard) {
+    const position = cordinates[0] + cordinates[1] * 10
+    const cell = document.querySelector(`.${id}>.board>.cell${position}`)
+    if (gameBoard.board[cordinates[0]][cordinates[1]])
+      cell.style.backgroundColor = 'rgb(230,0,0)'
+    else cell.style.backgroundColor = 'rgb(200,220,220)'
+    cell.appendChild(createCrossSvg())
+  }
+  return { showShipsPosition, showShipsCondition, showAttackResult }
 }
 
 module.exports = { Render }
